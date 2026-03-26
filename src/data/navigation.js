30b3dea91dca5c138914e32d66784a54b6e74860
Miroslaw <witcherofthorns@gmail.com>
2026-03-26 22:56:03 +0300

    start project, first work

---
+++ src/data/navigation.js
@@ -0,0 +1,6 @@
+export const navigation = [
+    { name: 'Главная', path: '/' },
+    { name: 'Политика', path: '/policy' },
+    { name: 'Условия', path: '/terms' },
+    { name: 'Блог', path: '/blog' }
+]
